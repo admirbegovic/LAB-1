732fcbe87e45fc9c1544844484cd70beb610cd54
admirbegovic <caketrainx@hotmail.com>
2013-09-19 20:25:15 +0200

    Lagt til nye filer og redigert. mer info i README

---
+++ Hierarkisk.js
@@ -0,0 +1,3 @@
+alert("Denne siden er midlertidig under konstruksjon og er ikke tigjengelig for øyeblikket. Takk for din tålmodighet!");
+   
+/*Gir et brukbart respons til brukeren om at siden er under konstruksjon i en alert box som kommer på siden*/
---
+++ Kronologisk.js
@@ -0,0 +1,3 @@
+alert("Denne siden er midlertidig under konstruksjon og er ikke tigjengelig for øyeblikket. Takk for din tålmodighet!");
+   
+/*Det kommer en popup boks som sier at siden er under konstruksjon. Slik vil brukeren få et brukbart respons.*/
